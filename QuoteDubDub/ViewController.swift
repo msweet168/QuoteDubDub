18a8fcd79337c3ac9e70edc2bf75e42eac58a7e3
msweet168 <mitchellsweet168@aol.com>
2017-04-04 21:25:56 -0400

    Initial Commit

---
+++ QuoteDubDub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuoteDubDub
+//
+//  Created by Mitchell Sweet on 4/4/17.
+//  Copyright © 2017 Mitchell Sweet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
